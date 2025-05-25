55ab148b536d982f228226657e1039a048ae341e
Avikumar Talaviya <88608935+avikumart@users.noreply.github.com>
2025-05-25 19:03:15 +0530

    bio and constant.py updates

---
+++ constant.py
@@ -0,0 +1,33 @@
+info = {
+   "Pronoun": "Him", 
+   "Name": "Avi",
+   "Full_Name": "Avi Kumar Talaviya",
+   "Intro": "A Data Scientist with a passion for solving real-world problems using data-driven insights.",
+   "LinkedIn": "https://www.linkedin.com/in/avi-kumar-talaviya-739153147/",
+   "GitHub": "https://github.com/avikumart",
+   "About":"Hey there, I'm Avi! I'm passionate about leveraging data and AI technology to drive meaningful insights and solutions in business.",
+   "Medium":"https://medium.com/@avikumart_",
+   "City":"Tucson, Arizona",
+   "Country":"United States",
+   "University": "University of Arizona",
+   "Degree": "Master of Science in Information Science with Specialization in Machine Learning",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/avi-kumar-talaviya-739153147/\"><img src=\"https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/profile.png" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "avikumart@arizona.edu"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@avikumart_"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
