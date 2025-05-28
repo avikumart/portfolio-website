0996130734f433469b254fc458f60f4933eaf08f
Avikumar Talaviya <88608935+avikumart@users.noreply.github.com>
2025-05-28 15:37:18 +0530

    review image

--- constant.py
+++ constant.py
@@ -33,7 +33,7 @@ embed_rss= {
 }
 
 endorsements = {
-    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img1": "https://mypfp.s3.ap-south-1.amazonaws.com/Screenshot+2025-05-28+at+3.34.46%E2%80%AFPM.png",
     "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
     "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
 }
